85f05ffd2432dfdd1e4c038976ec99177af59040
Abbas-Developer <abbas.ghulam.hack@gmail.com>
2026-01-11 10:06:34 +0500

    changessss

--- app.js
+++ app.js
@@ -21,28 +21,25 @@ function timestart() {
         if (s === 60) {
             s = 0;
             minutespan.textContent = m++;
+            console.log(m);
             if (m < 10) {
                 minutespan.textContent = "0" + m;
-                console.log(m);
-
             }
             else {
                 minutespan.textContent = m;
-                console.log(m);
             }
         }
         if (m === 60) {
             m = 0;
             hourspan.textContent = h++;
-            
-            
+            console.log(h);
+
+
             if (h < 10) {
                 hourspan.textContent = "0" + h;
-                console.log(h);
             }
             else {
                 hourspan.textContent = h;
-                console.log(h);
             }
         }
 
@@ -51,14 +48,14 @@ function timestart() {
 }
 
 
-function timestop(){
-     clearInterval(com)
+function timestop() {
+    clearInterval(com)
 }
 
-function timereset(){
-    h= 0;
-    s=0;
-    m=0;
+function timereset() {
+    h = 0;
+    s = 0;
+    m = 0;
     secondspan.textContent = "00";
     hourspan.textContent = "00";
     minutespan.textContent = "00";
