24493e56b74d537ebfa6bbb8a1c70121c35afbda
Abbas-Developer <abbas.ghulam.hack@gmail.com>
2026-01-11 10:04:00 +0500

    chnages

--- app.js
+++ app.js
@@ -21,23 +21,28 @@ function timestart() {
         if (s === 60) {
             s = 0;
             minutespan.textContent = m++;
-
             if (m < 10) {
                 minutespan.textContent = "0" + m;
+                console.log(m);
 
             }
             else {
                 minutespan.textContent = m;
+                console.log(m);
             }
         }
         if (m === 60) {
             m = 0;
             hourspan.textContent = h++;
+            
+            
             if (h < 10) {
                 hourspan.textContent = "0" + h;
+                console.log(h);
             }
             else {
                 hourspan.textContent = h;
+                console.log(h);
             }
         }
 
